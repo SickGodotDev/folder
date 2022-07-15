0de3d5b257aabbf464c500843d5061a96c58ef80
Insert Username here <107337675+SickGodotDev@users.noreply.github.com>
2022-07-15 13:35:34 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const { exec } = require("child_process");
+let filepath = "./index.bf"
 const execute = (command) => {exec(command, (error, stdout, stderr) => {if (error) {console.log(`error: ${error.message}`);return;}if (stderr) {console.log(`stderr: ${stderr}`);return;}console.log(stdout)})};
-if(!fs.existsSync("./index.bf")){fs.writeFileSync("./index.bf","");process.exit(1);}
-const code = fs.readFileSync("index.bf",{encoding: "utf8",flag:"r"});
+if(!fs.existsSync("./index.bf")){fs.writeFileSync(filepath,"");process.exit(1);}
+const code = fs.readFileSync(filepath,{encoding: "utf8",flag:"r"});
 let wtf = "#include <stdio.h>\n#include <unistd.h>\nint main(){ char array[30000]={0}; char *ptr=array;";
 let amount = 1;
 let type = code[0];
